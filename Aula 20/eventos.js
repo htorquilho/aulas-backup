90b596889ad5f9b529af6361938af3be7a909ecb
Henrique Torquilho Almeida <henriquetorquilho@protonmail.com>
2023-04-18 21:40:55 -0300

    Add files via upload

---
+++ Aula 20/eventos.js
@@ -0,0 +1,19 @@
+function cadastrar() {
+    fetch('https://api.site.com/veiculos'), {
+    method: 'GET',
+}};
+
+function listar() {
+    fetch('https://api.site.com/veiculos'), {
+    method: 'POST',
+}};
+
+function editar() {
+    fetch('https://api.site.com/veiculos'), {
+    method: 'PUT',
+}};
+
+function excluir() {
+    fetch('https://api.site.com/veiculos'), {
+    method: 'DELETE'
+}};
